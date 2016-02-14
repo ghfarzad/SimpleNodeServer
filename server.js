0331be2baf93e6cf439d2cb267c449fec10529a9
ghfarzad <ghfarzad@gmail.com>
2016-02-14 17:53:13 -0500

    Added express static middleware

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
 // set up 
 var express  = require('express');
 var bodyParser = require('body-parser');
+var path = require('path');
 
 var app  = express();
 
 // configuration
+app.use(express.static(path.join(__dirname, '../Client')));
 app.use(bodyParser.urlencoded({'extended':'true'}));
 app.use(bodyParser.json());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
 // application
 app.get('*', function(req, res) {
-    res.sendfile('./public/index.html');
+    res.sendFile('index.html', { root: path.join(__dirname, '../Client') });
 });
 
 
 // listen (start app with node server.js) 
 app.listen(8080);
-console.log("Server running at http://127.0.0.1:8000/");
+console.log("Server running at http://127.0.0.1:8080/");
